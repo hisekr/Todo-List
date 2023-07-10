b2c2c012b447787d5a3ad93d0d28107b135de439
Abhishek roshan <40202261+hisekr@users.noreply.github.com>
2023-07-10 21:12:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { TodoList } from "./TodoList";
+import { FaGithub } from "react-icons/fa";
+import "./styles.css";
+import { NewTodoForm } from "./NewTodoForm";
+
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ];
+    });
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <a
+        href="https://github.com/hisekr"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="github-icon"
+      >
+        <FaGithub />
+      </a>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  );
+}
---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export function NewTodoForm({ onSubmit }) {
+    const [newItem, setnewItem] = useState("")
+
+    function handleSubmit(e){
+        e.preventDefault()
+        if(newItem ==="") return
+
+        onSubmit(newItem)
+
+        setnewItem("")
+    }
+    return (
+        <form onSubmit={handleSubmit} className="new-item-form">
+            <div className="form-row">
+                <label htmlFor="item">New Item</label>
+                <input
+                  value={newItem}
+                  onChange={e => setnewItem(e.target.value)}
+                  type="text"
+                  id="item"
+                  />
+            </div>
+            <botton className="btn">Add</botton>
+        </form>
+    )
+}
